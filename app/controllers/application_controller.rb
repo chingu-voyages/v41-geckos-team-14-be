f15bb1e06268f73911f7f294faaaf2fdf2fca9a1
Cerdtruir <ghoornabeel@gmail.com>
2022-10-13 16:01:21 +0200

    Add JWT methods for authentication tokens

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,2 +1,25 @@
+require 'jwt'
+
 class ApplicationController < ActionController::API
+  SECRET = ENV['ENCODING_SECRET']
+
+  def authentication
+    decode_data = decode_user_data(request.headers['token'])
+    user_data = decode_data[0]['user_id'] if decode_data
+    user = User.find(user_data&.id)
+
+    render json: { message: 'invalid credentials' } unless user
+  end
+
+  def encode_user_data(payload)
+    JWT.encode payload, SECRET, 'HS256'
+  end
+
+  def decode_user_data(token)
+    JWT.decode token, SECRET, true, 'HS256'
+  rescue JWT::ExpiredSignature
+    render json: { message: 'token expired' }
+  rescue JWT::DecodeError
+    render json: { message: 'invalid token' }
+  end
 end
