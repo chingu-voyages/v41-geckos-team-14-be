4a0802cbeac861d63b8e544d56e2aa770a482b63
Cerdtruir <ghoornabeel@gmail.com>
2022-10-19 20:39:58 +0200

    Fix authorization to use official bearer specs

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -4,11 +4,11 @@ class ApplicationController < ActionController::API
   SECRET = ENV['ENCODING_SECRET']
 
   def authenticate_user
-    unless request.headers['token']
+    unless request.headers['Authorization']
       render status: 400, json: { message: 'invalid request' }
       return
     end
-    decoded_data = decode_user_data(request.headers['token'])
+    decoded_data = decode_user_data(request.headers['Authorization'].split(' ')[1])
     return decoded_data if decoded_data.include? 'Error:'
 
     decoded_data[0]['user_data']
